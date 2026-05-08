b4a3f832126f631839cc67b71a2b198cee3d3128
Tina Yan <tinayan@Tinas-MacBook-Air-5.local>
2026-05-07 20:02:02 -0400

    deleted the things in main() to call method in Main.java

--- src/FizzBuzz.java
+++ src/FizzBuzz.java
@@ -3,7 +3,7 @@
  */
 class FizzBuzz {
 
-    public static void main(String[] args) {
+    public static void main() {
 
         for (int i = 1; i < 100; i++) {
 
@@ -12,7 +12,7 @@ class FizzBuzz {
         }
     }
 
-    private static void doFizzBuzz(int i) {
+    public static void doFizzBuzz(int i) {
         boolean divisibleBy3 = i % 3 == 0;
         boolean divisibleBy5 = i % 5 == 0;
 

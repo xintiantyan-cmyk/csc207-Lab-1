0ccf1934294716734c0ca18f169dc67af56e78c5
Tina Yan <tinayan@Tinas-MacBook-Air-5.local>
2026-05-07 21:29:46 -0400

    started task 4

--- src/Multiples.java
+++ src/Multiples.java
@@ -1,4 +1,8 @@
-package PACKAGE_NAME;
-
 public class Multiples {
+    static void main() {
+        for (int i = 1; i <= 1000; i++) {
+            boolean divisibleBy3 = i % 3 == 0;
+            boolean divisibleBy5 = i % 5 == 0;
+        }
+    }
 }

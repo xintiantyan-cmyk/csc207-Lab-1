4b339e6b2568b6618ff03f940ec75f23d501432a
Tina Yan <tinayan@Tinas-MacBook-Air-5.local>
2026-05-07 22:10:21 -0400

    function that prints # of numtiples of 3 or 5 there are below 1000

--- src/Multiples.java
+++ src/Multiples.java
@@ -1,8 +1,15 @@
 public class Multiples {
     static void main() {
-        for (int i = 1; i <= 1000; i++) {
-            boolean divisibleBy3 = i % 3 == 0;
-            boolean divisibleBy5 = i % 5 == 0;
+        int total = 0; // the accumulator
+
+        for (int i = 3; i <= 1000; i++) {
+            boolean multipleOf3 = i % 3 == 0;
+            boolean multipleOf5 = i % 5 == 0;
+
+            if (multipleOf3 || multipleOf5) {
+                total += 1;
+            };
         }
+        System.out.println(total);
     }
 }
